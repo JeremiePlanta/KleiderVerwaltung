d4e0172d67b8a6625fb5133f2f5b9de38cddc289
JeremiePlanta <Jeremie.Planta@stahl-holding-saar.de>
2023-08-22 10:43:25 +0200

    Addend hinzugefügt zum Annotieren der Klassen gemäß der DDD Mustersprache Jexxa ArchitectureTests hinzugefügt zum überprüfen unsere Mustertreue, um eine 100% Mustertreue sicherzustellen und somit andere Entwickler beim verstehen des Codes zu unterstützen.

---
+++ src/test/java/architecture/ArchitectureTest.java
@@ -0,0 +1,44 @@
+package architecture;
+
+import org.example.Main;
+import org.junit.jupiter.api.Test;
+
+import static io.jexxa.jexxatest.architecture.ArchitectureRules.aggregateRules;
+import static io.jexxa.jexxatest.architecture.ArchitectureRules.patternLanguage;
+import static io.jexxa.jexxatest.architecture.ArchitectureRules.portsAndAdapters;
+
+/**
+ * This test can be used in all your applications.
+ * <p>
+ * You have only to adjust test validatePortsAndAdapters. Here you have to add all your
+ * packages containing the driven and driving adapters. This information is required to ensure
+ * that there are no dependencies between these packages.
+ */
+class ArchitectureTest {
+
+    @Test
+    void validatePortsAndAdapters()
+    {
+        portsAndAdapters(Main.class)
+                // Add all packages providing driven adapter
+                .addDrivenAdapterPackage("persistence")
+
+                // Add all packages providing driving adapter such as
+                // .addDrivingAdapterPackage("messaging")
+
+                .validate();
+    }
+
+
+    @Test
+    void validatePatternLanguage()
+    {
+        patternLanguage(Main.class).validate();
+    }
+
+    @Test
+    void validateAggregateRules()
+    {
+        aggregateRules(Main.class).validate();
+    }
+}
